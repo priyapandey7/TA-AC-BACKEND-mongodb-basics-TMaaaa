5cec5f608835fbc9b87bbf4feb90df05f0a2969a
priya pandey <priyarock746@gmail.com>
2021-12-16 21:35:56 +0530

    Assignment

---
+++ block-BMaaae/db.js
@@ -0,0 +1,32 @@
+var players = [
+    {
+        name:"player1", 
+        age: 21,
+        email:"player1@gmail.com",
+        bid_price: "22000k"
+    },
+    {
+        name:"player2", 
+        age: 23,
+        email:"player2@gmail.com",
+        bid_price: "23900k"
+    },
+    {
+        name:"player3", 
+        age: 25,
+        email:"player3@gmail.com",
+        bid_price: "26000k"
+    },
+    {
+        name:"player4", 
+        age: 22,
+        email:"player4@gmail.com",
+        bid_price: "22500k"
+    },
+    {
+        name:"player5", 
+        age: 21,
+        email:"player5@gmail.com",
+        bid_price: "22000k"
+    }
+]
---
+++ block-BMaaae/query.js
@@ -0,0 +1,10 @@
+// db.cricket.insertMany(players)
+// db.cricket.find().pretty()
+
+
+
+// db.football.insertMany(players)
+// db.TT.insertMany(players)
+
+// db.khokho.insertMany(players)
+
